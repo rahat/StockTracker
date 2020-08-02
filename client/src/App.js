1a6437fa0d8a0580f3e3054f6ab3226e520de68c
Rahat Mahmud <rahat.mahmud@macaulay.cuny.edu>
2020-08-02 03:27:00 -0400

    Add nav, favicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#">Stock Tracker</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#">Home</Nav.Link>
+          <Nav.Link href="#">Stocks</Nav.Link>
+          <Nav.Link href="#">Settings</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search..." className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
     </div>
-  );
+  )
 }
 
 export default App;
