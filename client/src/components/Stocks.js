e10e81feb53306e925fcb36b0cdddfcf4558ff85
Rahat Mahmud <rahat.mahmud@macaulay.cuny.edu>
2020-08-11 22:58:02 -0400

    Adding table for stocks

--- client/src/components/Stocks.js
+++ client/src/components/Stocks.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table'
 
 function Stocks() {
     return (
         <div>
-            <h1>Stocks</h1>
+            <h1 className="align-items-center">Stocks</h1>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Stock</th>
+                        <th>Ticker</th>
+                        <th>Open</th>
+                        <th>High</th>
+                        <th>Low</th>
+                        <th>Close</th>
+                        <th>Alert Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>...</td><td>...</td><td>...</td><td>...</td><td>...</td><td>...</td><td>...</td></tr>
+                </tbody>
+            </Table>
         </div>
     )
 }
